966fdcc7b4dec36fced9b982b3aa2fbbc075e055
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2020-03-11 09:58:36 -0400

    Add GMM

--- datasets.py
+++ datasets.py
@@ -40,3 +40,22 @@ class MNIST:
     def __str__(self):
         """Contains relevant dataset hyperparameters"""
         return f"<{self.__class__.__name__} object, {self.normalization}, {self.shape}>"
+
+
+class GMM:
+    def __init__(self, n_dims=2, n_clusters=2):
+        self.cluster_means = 0.1 + 0.8 * np.random.random((n_clusters, n_dims))
+        self.cluster_var = 0.05 + 0.05 * np.random.random((n_clusters, n_dims)) 
+        self.cluster_dist = np.random.exponential(1, (n_clusters,))
+        self.cluster_dist = self.cluster_dist / self.cluster_dist.sum()
+        self.cluster_cdf = np.cumsum(self.cluster_dist)
+
+    def sample_train(self, batch_size):
+        batch = []
+        for _ in range(batch_size):
+            cluster = np.random.random()
+            cluster = np.where(cluster < self.cluster_cdf)[0].min() # sampling from cdf
+            batch.append(np.clip(np.random.normal(
+                self.cluster_means[cluster,:],
+                self.cluster_var[cluster,:]), 0, 1))
+        return np.array(batch)
