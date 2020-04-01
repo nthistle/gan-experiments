a3ab92d3b31419e492ad28aa417cf7b4a6a6fd20
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2020-04-01 00:50:19 -0400

    Add GMM models

--- models.py
+++ models.py
@@ -38,3 +38,63 @@ class MNISTFullyConnectedDiscriminator(nn.Module):
         x = self.fc3(x)
         x = self.sigmoid(x)
         return x
+
+class GMMSimpleGenerator(nn.Module):
+    def __init__(self, latent_dim=4, n_dim=2):
+        super(GMMSimpleGenerator, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, (latent_dim+n_dim)//2)
+        self.fc2 = nn.Linear((latent_dim+n_dim)//2, n_dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
+
+class GMMSimpleDiscriminator(nn.Module):
+    def __init__(self, n_dim=2):
+        super(GMMSimpleDiscriminator, self).__init__()
+        self.fc1 = nn.Linear(n_dim, 3 * n_dim)
+        self.fc2 = nn.Linear(3 * n_dim, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
+
+class GMMDenseGenerator(nn.Module):
+    def __init__(self, latent_dim=256):
+        super(GMMDenseGenerator, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc2(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc3(x)
+        return x
+
+class GMMDenseDiscriminator(nn.Module):
+    def __init__(self):
+        super(GMMDenseDiscriminator, self).__init__()
+        self.fc1 = nn.Linear(2, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc2(x)
+        x = F.leaky_relu(x, 0.1)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        return x
